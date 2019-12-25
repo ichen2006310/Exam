7bbbeb561ed1fb71e7480254fac78b850b3b75f8
沈昱辰 <holliday@i-fit.com.tw>
2019-12-25 10:00:28 +0800

    commit exam_2

--- exam_2.php
+++ exam_2.php
@@ -1,4 +1,16 @@
 <?php
 
 $str = 15 ;
+$ar=array();
+for($i=1;$i<=$str;$i++){
+    if($i%3==0&&$i%5==0){
+        array_push($ar, $i);
+    }
+    elseif($i%3!=0&&$i%5!=0) {
+        array_push($ar, $i);
+    }
+}
 
+
+print_r($ar);
+print count($ar);
